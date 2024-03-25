278647fde8ec4de302d248fa50ee1549798aa0ae
Asen Dimitrov <dimtreo@gmail.com>
2024-03-25 15:45:02 +0200

    day 32 birthday wisher refactoring

--- day-032/birthday-wisher/main.py
+++ day-032/birthday-wisher/main.py
@@ -4,21 +4,18 @@ import datetime as dt
 
 from mailer.gmail import Gmail
 
-birthday_records = pandas.read_csv("birthdays.csv").to_dict(orient="records")
 templates = ["letter_templates/letter_1.txt",
              "letter_templates/letter_2.txt",
              "letter_templates/letter_3.txt"]
 
 today = dt.datetime.today()
+df = pandas.read_csv("birthdays.csv")
+today_records = df[(df['month'] == today.month) & (df['day'] == today.day)]
+mailer = Gmail("mailer/credentials.json")
 
-for record in birthday_records:
-    month = int(record["month"])
-    day = int(record["day"])
-    if day == today.day and month == today.month:
-        print(f"{month}-{day}")
-        template = random.choice(templates)
-        with open(template, "r") as text_file:
-            text = text_file.read()
-            text = text.replace("[NAME]", record["name"])
-            mailer = Gmail("mailer/credentials.json")
-            mailer.send_mail(record["email"], "Happy Birthday!", text)
+for index, row in today_records.iterrows():
+    template = random.choice(templates)
+    with open(template, "r") as text_file:
+        text = text_file.read()
+        text = text.replace("[NAME]", row["name"])
+        mailer.send_mail(row["email"], "Happy Birthday!", text)
